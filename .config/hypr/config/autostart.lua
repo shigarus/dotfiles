0ff731cf39cfb3b73039d1cd3ebd9ac4f1b9b946
Aleksandr Stovbyra <shiggarus@gmail.com>
2026-09-20 21:43:41 +0200

    cachyos hyprland conf

---
+++ .config/hypr/config/autostart.lua
@@ -0,0 +1,12 @@
+-- Auto-start config
+-- if you dont use UWSM add your auto start programs here, otherwise use XDG autostart https://wiki.archlinux.org/title/XDG_Autostart
+
+hl.on("hyprland.start", function()
+	hl.exec_cmd("dbus-update-activation-environment --systemd --all")
+	hl.exec_cmd("noctalia")
+	hl.exec_cmd("xhost +SI:localuser:root")
+	hl.exec_cmd("systemctl --user start hyprland-single-monitor.service")
+	hl.exec_cmd(TERMINAL, { workspace = "special:term" })
+	hl.exec_cmd(BROWSER, { workspace = "special:browser" })
+	-- hl.exec_cmd("telegram", {workspace="special:tg"})
+end)
---
+++ .config/hypr/config/workspaces.lua
@@ -0,0 +1,6 @@
+-- Workspace rules wiki https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+-- Add your workspace rules here. Increment the workspace number as you go. Do not have duplicate workspaces.
+hl.workspace_rule({ workspace = "name:default", default = true, layout = "Monocle" })
+hl.workspace_rule({ workspace = "name:term", persistent = true, layout = "Monocle" })
+hl.workspace_rule({ workspace = "name:browser", default = true, persistent = true })
+hl.workspace_rule({ workspace = "name:tg", persistent = true })
